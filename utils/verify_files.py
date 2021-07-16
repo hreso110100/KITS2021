31dd988f2f32e01629c9ea12643a548168eb2a19
hreso110100 <hreskodavid57@gmail.com>
2021-07-16 23:31:08 +0200

    add verify script

--- utils/verify_files.py
+++ utils/verify_files.py
@@ -1,23 +1,32 @@
-import matplotlib.pyplot as plt
+from copy import deepcopy
+
 import numpy as np
-import cv2
-from PIL.Image import Image
+from PIL import Image
+from numpy import copy, stack, moveaxis
 
 STORE_PATH = "C:\\Users\\Admin\\Desktop\\data"
-CASE = "case_0000"
+CASE = "case_00000"
 MASK = "aggregated_MAJ_seg"
 
 if __name__ == '__main__':
-    imaging = Image.open(f"{STORE_PATH}\\{CASE}\\imaging\\imaging.png")
-    imaging_np = np.array(imaging.getdata())
+    imaging = Image.open(f"{STORE_PATH}\\{CASE}\\imaging\\imaging_part236.png")
+    imaging_np = np.array(imaging, dtype=np.uint8)
+    imaging_np = stack((imaging_np, copy(imaging_np), copy(imaging_np)))
+    imaging_np = moveaxis(imaging_np, 0, -1)  # must be channel last to open via PIL
 
-    mask = Image.open(f"{STORE_PATH}\\{CASE}\\{MASK}\\mask_part90.png")
-    mask_np = np.array(mask.getdata())
+    mask = Image.open(f"{STORE_PATH}\\{CASE}\\{MASK}\\mask_part236.png")
+    mask_r = np.array(mask, dtype=np.uint8)
+    mask_g = deepcopy(mask_r)
+    mask_b = deepcopy(mask_r)
 
-    color = np.array([0, 255, 0], dtype='uint8')
+    # add color to mask
+    for index, mask in enumerate([mask_r, mask_g, mask_b]):
+        mask[mask == 1] = int(255 - ((index + 1) * 255))  # blue
+        mask[mask == 2] = int(255 - ((index + 1) * 128))  # green
+        mask[mask == 3] = int(255 - ((index + 1) * 64))  # orange
 
-    masked_img = np.where(mask_np[..., None], color, imaging_np)
-    out = cv2.addWeighted(imaging_np, 0.8, masked_img, 0.2, 0)
+    stacked = stack((mask_r, mask_g, mask_b))
+    stacked = moveaxis(stacked, 0, -1)  # must be channel last to open via PIL
 
-    plt.plot(out)
-    plt.show()
+    blended = Image.blend(Image.fromarray(imaging_np), Image.fromarray(stacked), alpha=0.5)
+    Image.show(blended)
