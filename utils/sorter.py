ea16373b5fddc8db901a5eb91d410d4d4c24546d
hreso110100 <hreskodavid57@gmail.com>
2021-07-18 23:08:08 +0200

    add sorter data script

--- utils/sorter.py
+++ utils/sorter.py
@@ -15,29 +15,26 @@ def create_dir(dir_name: str):
 
 
 if __name__ == '__main__':
+
+    # create dirs for storing sorted data
+    for cl_number in [1, 2, 3]:
+        create_dir(f"{STORE_PATH}\\{cl_number}\\imaging")
+        create_dir(f"{STORE_PATH}\\{cl_number}\\{MASK}")
+
     for case in os.listdir(BASE_PATH):
-        dir_1 = False
-        dir_2 = False
-        dir_3 = False
+        dir_number = 0
 
         for mask_file in sorted(os.listdir(f"{BASE_PATH}\\{case}\\{MASK}")):
             mask_np = np.array(Image.open(f"{BASE_PATH}\\{case}\\{MASK}\\{mask_file}"), dtype=np.uint8)
             if 2 in mask_np and 3 in mask_np:
-                dir_3 = True
+                dir_number = 3
             elif 2 in mask_np and 3 not in mask_np:
-                dir_2 = True
+                dir_number = 2
             elif 2 not in mask_np and 3 in mask_np:
-                dir_3 = True
+                dir_number = 3
             else:
-                dir_1 = True
+                dir_number = 1
 
-        # copy case to sort folder
-        if dir_1:
-            create_dir(f"{STORE_PATH}\\1")
-            copy_tree(f"{BASE_PATH}\\{case}", f"{STORE_PATH}\\1")
-        elif dir_2:
-            create_dir(f"{STORE_PATH}\\2")
-            copy_tree(f"{BASE_PATH}\\{case}", f"{STORE_PATH}\\2")
-        else:
-            create_dir(f"{STORE_PATH}\\3")
-            copy_tree(f"{BASE_PATH}\\{case}", f"{STORE_PATH}\\3")
+        # copy imaging and specific mask to sort folder
+        copy_tree(f"{BASE_PATH}\\{case}\\{MASK}", f"{STORE_PATH}\\{dir_number}\\{MASK}")
+        copy_tree(f"{BASE_PATH}\\{case}\\imaging", f"{STORE_PATH}\\{dir_number}\\imaging")
